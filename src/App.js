5f294bb5cee354a79facb9c9d662ca1f31350f6f
Dennis Ries <dtries@gmail.com>
2019-06-19 19:06:20 -0600

    updated main body lists

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react';
 import './App.css';
-import { Collapsible, CollapsibleItem, Navbar, Table } from 'react-materialize';
+import { Collapsible, CollapsibleItem, Navbar, NavItem, Table } from 'react-materialize';
 import TableBody from "./components/TableBody/tablebody";
 
 class App extends Component {
@@ -11,12 +11,12 @@ class App extends Component {
       list1: [{point: "Ties to Wisconsin and Upper Midwest"}, {point: "Family"}],
       list2: [{point: "Collaborative Approach Preferred, Multidimensional as Apropos"}, {point: "Team Building and Staff Development"}],
       list3: [{point: "Generated All Financial Statements and Provided Analysis for External and Internal Reporting"}, {point: "Forecasted Impact of New Accounting Pronouncements"}, {point: "Prepared Financial Information for External Surveys"}],
-      list4: [{point: "Managed Payments for Leased Properties for Target Corporation"}, {point: "Acted as Delegated Authority for Contract Approval for State Controller"}, {point: "Verified State Capital Appropriations Complied with State Statutes"}],
+      list4: [{point: "Managed Payments for Leased Properties for Target Corporation"}, {point: "Acted as Delegated Authority for Contract Approval for State Controller"}, {point: "Oversaw Cash Flow for Capital Construction Projects"}],
       list5: [{point: "Reported Investment Activity to Board of Trustees"}, {point: "Chaired the Defined Contribution Plan Investment Committee"}, {point: "Allocated Spending Authorization and Monitored Compliance"}],
       list6: [{point: "Designed and Implemented Administrative Shared Services"}, {point: "Established Change Agent Network"}],
       list7: [{point: "Managed Budget, Forecast, and Requests"}, {point: "Oversaw Expenditure Compliance and Allowability"}, {point: "Enacted Deficit Account Resolution"}],
-      list8: [{point: "Designed Financial Operations Websites"}, {point: "Led Function and Testing of Financial and HR Data Systems"}, {point: "Implemented New Travel and Expense Management System"}],
-      list9: [{point: "Developed, Revised, and Enforced University Fiscal Policy and Procedures"}, {point: "Ensured University Compliance with Laws and Regulations"}, {point: "Mitigated Risk through Policy and Insurance Review"}],
+      list8: [{point: "Ensured Compliance with Employment Related Laws and Regulations"}, {point: "Managed Financial Aspects of Benefit Plans"}, {point: "Led Onboarding Activities for New Hires"}],
+      list9: [{point: "Developed, Revised, and Enforced University Fiscal Policy and Procedures"}, {point: "Engineered Enterprise Risk Management Structure, Process, and Implementation"}, {point: "Mitigated Risk through Policy and Insurance Review"}],
       list10: [{point: "World Class Liberal Arts Institution"}, {point: "Development of the Whole Person"}, {point: "Value Tradition While Forward Thinking"}]
     };
   }
@@ -31,13 +31,13 @@ class App extends Component {
         </span>  
         } 
         fixed={true}
-        centerLogo={true}
-        // { ... 'right'}
-        >
-      </Navbar>
-      <div style={{ position: "absolute"}}>
-        <br />
-      </div>
+        centerLogo={true}>
+        
+        <NavItem 
+        href="#" className="snc-logo">
+          &nbsp;
+        </NavItem>
+       </Navbar>
       <div className="container">     
         <Collapsible popout>
             <CollapsibleItem header="Hello" icon="done_outline">
@@ -100,17 +100,7 @@ class App extends Component {
                 )}
               </Table>          
             </CollapsibleItem>
-            <CollapsibleItem header="Budget & Appropriations" icon="done_outline">
-              <Table className="table_class">
-                {this.state.list7.map((listInfo, index) => 
-                  <TableBody
-                    key = {index}
-                    listItem = {listInfo.point}
-                  />
-                )}
-              </Table>
-            </CollapsibleItem>
-            <CollapsibleItem header="Information Systems Administration" icon="done_outline">
+            <CollapsibleItem header="Human Resources" icon="done_outline">
               <Table className="table_class">
                 {this.state.list8.map((listInfo, index) => 
                   <TableBody
@@ -119,6 +109,16 @@ class App extends Component {
                   />
                 )}
               </Table>         
+            </CollapsibleItem>
+            <CollapsibleItem header="Budget" icon="done_outline">
+              <Table className="table_class">
+                {this.state.list7.map((listInfo, index) => 
+                  <TableBody
+                    key = {index}
+                    listItem = {listInfo.point}
+                  />
+                )}
+              </Table>
             </CollapsibleItem>
             <CollapsibleItem header="Policy, Compliance, & Risk Management" icon="done_outline">
               <Table className="table_class">
